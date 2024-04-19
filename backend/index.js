69ac75070ff24f403f8eea561d0c0f770c1a1630
aishwarya14-dev <aishwaryamehrotra14@gmail.com>
2024-04-20 02:07:08 +0530

    backend part 2

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,23 @@
 const express = require("express");
 
+const mainRouter = require("./routes/index");
+
+const app = express();
+
+app.use("/api/v1",mainRouter);
+
+
+
+// /api/v1/user/sign-up
+// /api/v1/user/signin
+// /api/1v/user/changePassword....
+
+// route user requests to user router and account requests to account router
+
+
+// /api/v1/account/transferMoney
+// /api/v1/account/balance
+
+//
+
 
---
+++ backend/routes/account.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+const router = express.Router();
+
+module.exports = router;
